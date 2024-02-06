5e29583605b35ad54e44b5abbc30906266897aaa
maelclou11 <maelclou11@hotmail.com>
2024-02-05 20:26:31 -0500

    database change3

--- database.js
+++ database.js
@@ -1,15 +1,9 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  protocol: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  }
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
 });
 
 module.exports = sequelize;
